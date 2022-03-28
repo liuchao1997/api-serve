837e2368386c29cb047395c8e980bbdc4b4601f7
liuchao1997 <102522976+liuchao1997@users.noreply.github.com>
2022-03-28 21:19:52 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: false,
   transpileDependencies: true,
+  transpileDependencies: false,
   transpileDependencies: false
-
 })
